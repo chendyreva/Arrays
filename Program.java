f9bbf48ee92f7f76d98b4ccfc70f68a1d70b6872
chendyreva <nat-217@mail.ru>
2018-11-30 21:45:48 +0500

    Arrays

---
+++ Program.java
@@ -0,0 +1,20 @@
+package ru.geekbrains;
+
+public class Program {
+final static int  n = 10;
+
+    public static void main(String[] args) {
+        MyArrayList<Character> list = new MyArrayList<>();
+        System.out.println();
+//        list.insert('b');
+//        list.insert('a');
+//        list.insert('c');
+//        list.insert('d');
+//        list.insert('k');
+//
+//        System.out.println(list.find('x'));
+//        System.out.println(list.find('n'));
+//        System.out.println(list.size);
+
+    }
+}
