3b49320b47864e995ded74a0c469e900be0fbea5
chendyreva <nat-217@mail.ru>
2018-12-01 21:34:20 +0500

    удаление элемента и создание массива (1000000) элементов

--- Program.java
+++ Program.java
@@ -1,20 +1,37 @@
 package ru.geekbrains;
 
 public class Program {
-final static int  n = 10;
 
     public static void main(String[] args) {
-        MyArrayList<Character> list = new MyArrayList<>();
-        System.out.println();
-//        list.insert('b');
-//        list.insert('a');
-//        list.insert('c');
-//        list.insert('d');
-//        list.insert('k');
-//
-//        System.out.println(list.find('x'));
-//        System.out.println(list.find('n'));
-//        System.out.println(list.size);
+            int[] arr = {27,33,333,400,200,600,800,20,7,4,3,2};
+        int i;
+        int len = arr.length;
+        int search = 5;
 
+///////////////1 delete
+        System.out.println("Выводим массив");
+        for(int j = 0; j < len; j++){
+            System.out.println(arr[j]);
+        }
+
+        System.out.println("Поиск искомого элемента");
+        for(i = 0;i < len;i++){
+            if (arr[i] == search) break;
+        }
+        System.out.println("Сдвиг всех элементов на 1 шаг влево");
+        for (int j = i; j<len-1; j++){
+            arr[j] = arr[j+1];
+        }
+        len--;
+
+        System.out.println("Вывод массива с удаленным элементом");
+        for(int j = 0; j < len; j++){
+            System.out.println(arr[j]);
+        }
+
+////////////////1 выводим 1000000 массив
+//        for(int i = 0;i < 1000000;i++) {
+//            System.out.println(arr[i]);
+ //       }
     }
 }
